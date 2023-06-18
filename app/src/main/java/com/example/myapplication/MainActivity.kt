ef13fbf8c2f0e75bb341b34483ed55b924a27aad
Anastasia_Tylenkova <anastasia_tyl@mail.ru>
2023-06-18 21:27:36 +0300

    24_module

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,12 +7,80 @@ import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.content.res.Resources
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.FilmListRecyclerAdapter
+import com.example.myapplication.Film
+import com.example.myapplication.FilmViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
     @SuppressLint("MissingInflatedId")
+    val filmsDataBase: List<Film> = listOf(
+        Film("Halloween", R.drawable.image_10, "Fifteen years after murdering his sister on Halloween night 1963, Michael Myers escapes from a mental hospital and returns to the small town of Haddonfield, Illinois to kill again."),
+        Film("Scream 6", R.drawable.image_1, "In the next installment, the survivors of the Ghostface killings leave Woodsboro behind and start a fresh chapter in New York City."),
+        Film("Alice in Wonderland", R.drawable.image_8, "Nineteen-year-old Alice returns to the magical world from her childhood adventure, where she reunites with her old friends and learns of her true destiny: to end the Red Queen's reign of terror."),
+        Film("Interstellar", R.drawable.image_7, "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans."),
+        Film("Dark Knight", R.drawable.image_6, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."),
+        Film("Inception", R.drawable.image_5, "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."),
+        Film("Mandalorian", R.drawable.image_11, "The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic."),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        initNavigation()
+
+        main_recycler.apply {
+
+            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                override fun click(film: Film) {
+
+                    val bundle = Bundle()
+
+                    bundle.putParcelable("film", film)
+
+                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+
+                    intent.putExtras(bundle)
+
+                    startActivity(intent)
+                }
+            })
+
+            adapter = filmsAdapter
+
+            layoutManager = LinearLayoutManager(this@MainActivity)
+
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+        }
+        //Кладем нашу БД в RV
+        filmsAdapter.addItems(filmsDataBase)
+    }
+
+    private fun initNavigation() {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
 
         val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
         topAppBar.setOnMenuItemClickListener {
@@ -52,3 +120,16 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+class TopSpacingItemDecoration (private val paddingInDp: Int): RecyclerView.ItemDecoration() {
+    private val Int.convertPx: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = paddingInDp.convertPx
+        outRect.right = paddingInDp.convertPx
+        outRect.left = paddingInDp.convertPx
+
+    }
+}
+
